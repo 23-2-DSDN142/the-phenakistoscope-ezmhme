cafd3e860755f7d773e78477bfcfa0f9a83c1760
ezmhme <emzhemingway@gmail.com>
2023-09-07 11:22:17 +1200

    Edit

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -1,4 +1,4 @@
-const SLICE_COUNT = 10;
+const SLICE_COUNT = 12;
 
 function setup_pScope(pScope){
   pScope.output_mode(ANIMATED_DISK);
@@ -6,14 +6,15 @@ function setup_pScope(pScope){
   pScope.draw_layer_boundaries(true);
   pScope.set_direction(CCW);
   pScope.set_slice_count(SLICE_COUNT);
+  pScope.load_image("jellyfish" , "png");
 }
 
 function setup_layers(pScope){
 
   new PLayer(null, 220);  //lets us draw the whole circle background, ignoring the boundaries
 
-  var layer1 = new PLayer(faces);
-  layer1.mode( SWIRL(5) );
+  var layer1 = new PLayer(jellyfish);
+  layer1.mode( SWIRL(3.2) );
   layer1.set_boundary( 200, 1000 );
 
   var layer2 = new PLayer(squares);
@@ -21,15 +22,21 @@ function setup_layers(pScope){
   layer2.set_boundary( 0, 400 );
 }
 
-function faces(x, y, animation, pScope){
+function jellyfish(x, y, animation, pScope){
   
-  scale(animation.frame*2);
+  scale(0.1);
+  pScope.draw_image("jellyfish",x,y);
 
-  ellipse(0,0,50,50); // draw head
-  fill(30);
-  ellipse(-10,-10,10,10); //draw eye
-  ellipse(10,-10,10,10); // draw eye
-  arc(0,10,20,10,0,180); // draw mouth
+  //scale(animation.frame*2);
+
+  //ellipse(0,0,50,50); // draw head
+  //fill(30);
+  //ellipse(-10,-10,10,10); //draw eye
+  //ellipse(10,-10,10,10); // draw eye
+  //arc(0,10,20,10,0,180); // draw mouth
+  //fill(255)
+  //ellipse(15,15,50,50)
+  //ellipse(-30,0,50,50)
 
 }
 
@@ -40,10 +47,12 @@ function squares(x, y, animation, pScope){
   let backgroundArcStart = 270 - angleOffset;
   let backgroundArcEnd = 270 + angleOffset;
 
-  fill(66, 135, 245)
+  fill(255)
   arc(x,y,800,800,backgroundArcStart,backgroundArcEnd); // draws "pizza slice" in the background
 
-  fill(255)
-  rect(-10,-300-animation.wave()*50,20,20) // .wave is a cosine wave btw
+  //fill(255)
+  //rect(10,-300,animation.wave*50,20,20) // .wave is a cosine wave btw
 
+  //ellipse(300,10,30,30)
+  ellipse(5,-250-animation.wave()*25,75,75) // .wave is a cosine wave btw
 }
