aae510a045aa372590c96b05514ac0765868e82e
ezmhme <emzhemingway@gmail.com>
2023-09-10 12:23:51 +1200

    Updates

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -6,39 +6,40 @@ function setup_pScope(pScope){
   pScope.draw_layer_boundaries(true);
   pScope.set_direction(CCW);
   pScope.set_slice_count(SLICE_COUNT);
-  pScope.load_image("jellyfish" , "png");
+  //pScope.load_image("jellyfish" , "png");
+  pScope.load_image_sequence("jellyfish" , "png", 12);
 }
 
 function setup_layers(pScope){
 
-  new PLayer(null, 220);  //lets us draw the whole circle background, ignoring the boundaries
+  new PLayer(null, 255);  //lets us draw the whole circle background, ignoring the boundaries
 
-  var layer1 = new PLayer(jellyfish);
-  layer1.mode( SWIRL(3.2) );
-  layer1.set_boundary( 200, 1000 );
+  var jellyfishSequence = new PLayer(jellyfish);
+  jellyfishSequence.mode(RING);
+  jellyfishSequence.set_boundary(0,1000);
+
+  //var layer1 = new PLayer(jellyfish);
+  //layer1.mode(RING);
+  //layer1.set_boundary( 200, 1000 );
 
   var layer2 = new PLayer(squares);
-  layer2.mode( RING );
-  layer2.set_boundary( 0, 400 );
+  layer2.mode( SWIRL(1) );
+  layer2.set_boundary(0,100);
 }
 
 function jellyfish(x, y, animation, pScope){
-  
-  scale(0.1);
-  pScope.draw_image("jellyfish",x,y);
 
-  //scale(animation.frame*2);
-
-  //ellipse(0,0,50,50); // draw head
-  //fill(30);
-  //ellipse(-10,-10,10,10); //draw eye
-  //ellipse(10,-10,10,10); // draw eye
-  //arc(0,10,20,10,0,180); // draw mouth
-  //fill(255)
-  //ellipse(15,15,50,50)
-  //ellipse(-30,0,50,50)
+  scale(2);
+  pScope.draw_image_from_sequence("jellyfish", 0, -280-animation.wave(1)*20, animation.frame*0.5);
 
 }
+
+//function jellyfish(x, y, animation, pScope){
+  
+  //scale(0.2);
+  //pScope.draw_image("jellyfish",-20,-2750-animation.wave(1)*180);
+
+//}
 
 function squares(x, y, animation, pScope){
 
@@ -47,12 +48,12 @@ function squares(x, y, animation, pScope){
   let backgroundArcStart = 270 - angleOffset;
   let backgroundArcEnd = 270 + angleOffset;
 
-  fill(255)
-  arc(x,y,800,800,backgroundArcStart,backgroundArcEnd); // draws "pizza slice" in the background
+  //fill(255)
+  //arc(x,y,800,800,backgroundArcStart,backgroundArcEnd); // draws "pizza slice" in the background
 
   //fill(255)
   //rect(10,-300,animation.wave*50,20,20) // .wave is a cosine wave btw
 
   //ellipse(300,10,30,30)
-  ellipse(5,-250-animation.wave()*25,75,75) // .wave is a cosine wave btw
+  //ellipse(0,-50-animation.wave(2)*20,45,45) // .wave is a cosine wave btw
 }
