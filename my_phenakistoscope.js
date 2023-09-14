4247ef908ee739d22240f4531e74775333a5659e
ezmhme <emzhemingway@gmail.com>
2023-09-14 14:07:09 +1200

    Final Submission

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -3,7 +3,7 @@ const SLICE_COUNT = 13;
 function setup_pScope(pScope){
   pScope.output_mode(ANIMATED_DISK);
   pScope.scale_for_screen(true);
-  pScope.draw_layer_boundaries(true);
+  pScope.draw_layer_boundaries(false);
   pScope.set_direction(CCW);
   pScope.set_slice_count(SLICE_COUNT);
   pScope.load_image_sequence("jellyfish" , "png", 12);
@@ -12,44 +12,62 @@ function setup_pScope(pScope){
 }
 
 function setup_layers(pScope){
+  //new PLayer(null, 255);  //lets us draw the whole circle background, ignoring the boundaries
 
-  new PLayer(null, 255);  //lets us draw the whole circle background, ignoring the boundaries
+  var background = new PLayer(backgroundlayer);
+  background.mode(RING);
 
-  var backgroundlayer = new PLayer(backgroundlayer);
-  backgroundlayer.mode(RING);
+  var border = new PLayer(coral);
+  border.mode(RING);
+  border.set_boundary(0,0);
 
-  var coral = new PLayer(coral);
-  coral.mode(RING)
+  var bubblelayer = new PLayer(bubble);
+  bubblelayer.mode(SWIRL(10));
+  bubblelayer.set_boundary(0, 1000);
 
   var jellyfishSequence = new PLayer(jellyfish);
   jellyfishSequence.mode(RING);
-  jellyfishSequence.set_boundary(0,1000);
+  jellyfishSequence.set_boundary(0,0);
 
-  //var layer2 = new PLayer(squares);
-  //layer2.mode(RING);
-  //layer2.set_boundary(0,100);
+  angleMode(DEGREES)
+}
+
+function backgroundlayer(x , y, animation, pScope){
+  push();
+  if(animation.frame<1/SLICE_COUNT){
+    scale(0.8)
+    pScope.draw_image("backgroundlayer", 0, 0);
+  }
+  pop();
+}
+
+function bubble(x, y, animation, pScope){
+  rotate(1000*animation.frame);
+  scale(1*animation.frame);
+  stroke(190, 250, 249);
+  strokeWeight(10);
+  noFill();
+  ellipse(250, 0, 85, 85);
+  fill(190, 250, 249, 125);
+  ellipse(250, 0, 85, 85);
+
+  noFill();
+  stroke(190, 250, 249);
+  strokeWeight(10);
+  strokeCap(ROUND);
+  arc(247.5, -2.5, 50, 50, 0, 90);
 }
 
 function jellyfish(x, y, animation, pScope){
-  //pScope.draw_image("backgroundlayer",x,y);
-
+  push();
   scale(1.5);
   pScope.draw_image_from_sequence("jellyfish", 0, -270-animation.wave(1)*20, animation.frame*0.5);
-
-  scale(0.15);
-  pScope.draw_image("coral", 1300-animation.wave(1)*15, 2700);
+  pop();
 }
 
-//function squares(x, y, animation, pScope){
-
-  // this is how you set up a background for a specific layer
-  //let angleOffset = (360 / SLICE_COUNT) / 2
-  //let backgroundArcStart = 270 - angleOffset;
-  //let backgroundArcEnd = 270 + angleOffset;
-
-  //fill(50)
-  //arc(x,y,300,300,backgroundArcStart,backgroundArcEnd); // draws "pizza slice" in the background
-
-  //fill(255)
-  //rect(0,0,animation.frame) // .wave is a cosine wave btw
-//}
+function coral(x, y, animation, pScope){
+  push();
+  scale(1.7);
+  pScope.draw_image("coral", 125-animation.wave(1)*15, 465);
+  pop();
+}
